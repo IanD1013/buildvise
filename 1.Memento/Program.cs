0930a80dd2d4aa3bdacfa04de29f4c4d3c7824c2
IanD1013 <ian.tung1013@gmail.com>
2024-11-08 22:20:09 +1300

    Implement memento basic example

---
+++ 1.Memento/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using _1.Memento;
+
+Originator originator = new();
+Caretaker caretaker = new();
+
+originator.SetState("State 1");
+caretaker.AddMemento(originator.CreateMemento());
+
+originator.SetState("State 2");
+caretaker.AddMemento(originator.CreateMemento());
+
+originator.SetState("State 3");
+caretaker.AddMemento(originator.CreateMemento());
+
+originator.SetState("State 4");
+caretaker.AddMemento(originator.CreateMemento());
+
+originator.Restore(caretaker.GetMemento(0));
+
+Console.WriteLine(originator.GetState());
