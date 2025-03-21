8b293fedf30c792886e7e266f69ab5f1fbf127d5
IanD1013 <ian.tung1013@gmail.com>
2025-03-21 23:08:01 +1300

    Linq - chain multiple where clauses

--- Linq.Cmd/3_FilteringAndOrdering/WhereConditions.cs
+++ Linq.Cmd/3_FilteringAndOrdering/WhereConditions.cs
@@ -73,6 +73,14 @@ public class WhereConditions : QueryRunner
     private void MultipleConditions_Q()
     {
         var sourceMovies = Repository.GetAllMovies();
+        
+        var result = 
+            from movie in sourceMovies
+            where movie.Name.Contains("Spider-Man")
+            where movie.ReleaseDate.Year < 2020
+            select movie;
+        
+        PrintAll(result);
     }
     
     /// <summary>
@@ -81,5 +89,11 @@ public class WhereConditions : QueryRunner
     private void MultiplesConditions_F()
     {
         var sourceMovies = Repository.GetAllMovies();
+
+        var result = sourceMovies
+            .Where(movie => movie.Name.Contains("Iron"))
+            .Where(movie => movie.ReleaseDate.Year < 2020);
+        
+        PrintAll(result);
     }
 }
