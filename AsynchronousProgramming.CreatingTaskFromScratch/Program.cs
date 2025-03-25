77388a29d7efb7c240fbc15c46b90352ad9a7b84
IanD1013 <ian.tung1013@gmail.com>
2025-03-25 18:58:59 +1300

    Asynchronous programming - create async from scratch - Delay

--- AsynchronousProgramming.CreatingTaskFromScratch/Program.cs
+++ AsynchronousProgramming.CreatingTaskFromScratch/Program.cs
@@ -4,7 +4,11 @@ Console.WriteLine($"Starting Thread Id: {Environment.CurrentManagedThreadId}");
 
 DomeTrainTask.Run(() => Console.WriteLine($"First DomeTrainTask Id: {Environment.CurrentManagedThreadId}")).Wait();
 
+DomeTrainTask.Delay(TimeSpan.FromSeconds(1)).Wait();
+
 Console.WriteLine($"Second DomeTrainTask Id: {Environment.CurrentManagedThreadId}");
+
+DomeTrainTask.Delay(TimeSpan.FromSeconds(1)).Wait();
 
 DomeTrainTask.Run(() => Console.WriteLine($"Third DomeTrainTask Id: {Environment.CurrentManagedThreadId}")).Wait();
 
